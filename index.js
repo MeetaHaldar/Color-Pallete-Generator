84836a69f033ca31559ca7298cc49fea93db952b
meeta haldar <meetahaldar1001@gmail.com>
2021-10-07 21:14:40 +0530

    added js constants

---
+++ index.js
@@ -0,0 +1,5 @@
+const colorCard = document.querySelectorAll(".color-card");
+const colorCards = Array.from(colorCard);
+const generateBtn = document.querySelector("#generate");
+const closePopup = document.querySelector(".fa-times");
+const popup = document.querySelector(".popup");
