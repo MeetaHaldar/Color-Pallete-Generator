e2e8be0035e2bda9ea4149954b29929efc6d0b33
meeta haldar <meetahaldar1001@gmail.com>
2021-10-08 23:39:38 +0530

    maked generateRandomHexColor constant for generating color

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const colorCards = Array.from(colorCard);
 const generateBtn = document.querySelector("#generate");
 const closePopup = document.querySelector(".fa-times");
 const popup = document.querySelector(".popup");
+
+const generateRandomHexColor = () => {
+    let color = '#';
+    for (let i = 0; i < 6; i++){
+       const random = Math.random();
+       const bit = Math.floor((random * 16));
+       color += (bit).toString(16);
+    };
+    return color;
+}
